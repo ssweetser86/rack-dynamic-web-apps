06976f374f4c304bdc2585a6e164dffda43be988
Stephen <stephenjsweets@gmail.com>
2021-01-12 12:47:31 -0500

    commit tree b687a11aa47ab229b807ae513dbedbf75294df74

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,19 @@
 class Application
 
+  def rand
+    Kernel.rand(1..20)
+  end
+
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    n1, n2, n3 = rand, rand, rand
+
+    if n1 == n2 && n2 == n3
+      resp.write "#{n1} | #{n2} | #{n3}\n\nYou Win"
+    else
+      resp.write "#{n1} | #{n2} | #{n3}\n\nYou Lose"
+    end
+    
     resp.finish
   end
 
